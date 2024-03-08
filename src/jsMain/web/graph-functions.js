a9204472ab12d69b67d5a15b59a99cae1b8a8c27
Zerumi <46845708+Zerumi@users.noreply.github.com>
2024-03-08 20:27:47 +0300

    fix: remove unnecessary check in graph.js

--- src/jsMain/web/graph-functions.js
+++ src/jsMain/web/graph-functions.js
@@ -1,4 +1,3 @@
-let activated = false
 let elt;
 let calculator;
 
--- src/jsMain/web/graph.js
+++ src/jsMain/web/graph.js
@@ -1,23 +1,20 @@
-if (!activated) {
-    elt = document.getElementById('graph');
-    calculator = Desmos.GraphingCalculator(elt, {
-        keypad: false,
-        expressions: false,
-        settingsMenu: false,
-        invertedColors: true,
-        xAxisLabel: 'x',
-        yAxisLabel: 'y',
-        xAxisStep: 1,
-        yAxisStep: 1,
-        xAxisArrowMode: Desmos.AxisArrowModes.POSITIVE,
-        yAxisArrowMode: Desmos.AxisArrowModes.POSITIVE
-    });
+elt = document.getElementById('graph');
+calculator = Desmos.GraphingCalculator(elt, {
+    keypad: false,
+    expressions: false,
+    settingsMenu: false,
+    invertedColors: true,
+    xAxisLabel: 'x',
+    yAxisLabel: 'y',
+    xAxisStep: 1,
+    yAxisStep: 1,
+    xAxisArrowMode: Desmos.AxisArrowModes.POSITIVE,
+    yAxisArrowMode: Desmos.AxisArrowModes.POSITIVE
+});
 
-    calculator.setMathBounds({
-        left: -5, right: 5, bottom: -5, top: 5
-    });
+calculator.setMathBounds({
+    left: -5, right: 5, bottom: -5, top: 5
+});
 
-    let newDefaultState = calculator.getState();
-    calculator.setDefaultState(newDefaultState);
-
-}
+let newDefaultState = calculator.getState();
+calculator.setDefaultState(newDefaultState);
